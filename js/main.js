65b7d601cd5605b1e2bdf7e8d52b38184171e37d
Rachel Glennie <glen0038@algonquinlive.com>
2016-03-15 14:12:01 -0400

    Ready to go

--- js/main.js
+++ js/main.js
@@ -3,3 +3,24 @@ var $body = $('body');
 var $belowList = $('<ul>');
 var $aboveHeading = $('<h1>');
 var $aboveList = $('<ul>');
+
+
+$belowHeading.html('Below ground veggies');
+
+$body.append($belowHeading);
+
+$body.append($belowList);
+
+$aboveHeading.html('Above ground veggies');
+
+$body.append($aboveHeading);
+
+$body.append($aboveList);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $belowList.append($(this));
+  } else {
+    $aboveList.append($(this));
+  }
+});
